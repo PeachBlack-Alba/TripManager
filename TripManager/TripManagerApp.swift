4618936d51d573826c3870f1839ba4b2c642fe9f
PeachBlack-Alba <peachblackweb@gmail.com>
2024-06-20 10:54:54 +0200

    Initial Commit

---
+++ TripManager/TripManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripManagerApp.swift
+//  TripManager
+//
+//  Created by Alba Torres Rodriguez on 20.06.24.
+//
+
+import SwiftUI
+
+@main
+struct TripManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
