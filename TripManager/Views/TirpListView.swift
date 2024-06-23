5112c9e4f0a9e3ceffc8f07a531a8d0d166a4990
PeachBlack-Alba <peachblackweb@gmail.com>
2024-06-23 19:33:53 +0200

    Abstraction of TirpListView

--- TripManager/Views/TirpListView.swift
+++ TripManager/Views/TirpListView.swift
@@ -1,51 +1,51 @@
-////
-////  TirpListView.swift
-////  TripManager
-////
-////  Created by Alba Torres Rodriguez on 23.06.24.
-////
 //
-//import Foundation
-//import SwiftUI
-//import MapKit
+//  TirpListView.swift
+//  TripManager
 //
-//struct TripListView: View {
-//    @ObservedObject var viewModel: TripListViewWrapper
+//  Created by Alba Torres Rodriguez on 23.06.24.
 //
-//    var body: some View {
-//        NavigationView {
-//            VStack {
-//                MapView(viewModel: viewModel.mapViewModel)
-//                    .frame(height: UIScreen.main.bounds.height / 2)
-//
-//                List(viewModel.trips.indices, id: \.self) { index in
-//                    let trip = viewModel.trips[index]
-//                    VStack(alignment: .leading) {
-//                        Text(trip.description)
-//                            .font(.headline)
-//                        Text("Driver: \(trip.driverName)")
-//                            .font(.subheadline)
-//                        Text("Start: \(trip.startTime)")
-//                            .font(.caption)
-//                        Text("End: \(trip.endTime)")
-//                            .font(.caption)
-//                    }
-//                    .onTapGesture {
-//                        viewModel.presenter?.didSelectTrip(trip)
-//                    }
-//                }
-//                .navigationBarTitle("Trips")
-//                .onAppear {
-//                    print("View: onAppear")
-//                    viewModel.presenter?.viewDidLoad()
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//struct TripListView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        TripListRouter.createTripListModule()
-//    }
-//}
+
+import Foundation
+import SwiftUI
+import MapKit
+
+struct TripListView: View {
+    @ObservedObject var viewModel: TripListViewWrapper
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                MapView(viewModel: viewModel.mapViewModel)
+                    .frame(height: UIScreen.main.bounds.height / 2)
+
+                List(viewModel.trips.indices, id: \.self) { index in
+                    let trip = viewModel.trips[index]
+                    VStack(alignment: .leading) {
+                        Text(trip.description)
+                            .font(.headline)
+                        Text("Driver: \(trip.driverName)")
+                            .font(.subheadline)
+                        Text("Start: \(trip.startTime)")
+                            .font(.caption)
+                        Text("End: \(trip.endTime)")
+                            .font(.caption)
+                    }
+                    .onTapGesture {
+                        viewModel.presenter?.didSelectTrip(trip)
+                    }
+                }
+                .navigationBarTitle("Trips")
+                .onAppear {
+                    print("View: onAppear")
+                    viewModel.presenter?.viewDidLoad()
+                }
+            }
+        }
+    }
+}
+
+struct TripListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TripListRouter.createTripListModule()
+    }
+}
